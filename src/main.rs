34cae842df1adbdfc7aef205c1d8e98d6b1ac635
luckycoder <luckycoder@126.com>
2025-10-25 10:56:26 +0800

    feat:增加静态文件路由

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,29 @@ use axum::{
     Router,
     extract::{Path, Query},
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{get, get_service},
 };
 use serde::Deserialize;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new()
         .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+        .route("/hello2/:name", get(handler_hello2))
+        .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> listening on {addr}\n");
 
     let listener = TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, routes_hello).await.unwrap();
+}
+
+// 静态文件路由
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
 #[derive(Debug, Deserialize)]
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,5 +6,6 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
     hc.do_get("/hello?name=John").await?.print().await?;
     hc.do_get("/hello2/Mi").await?.print().await?;
+    hc.do_get("/src/main.rs").await?.print().await?;
     Ok(())
 }
